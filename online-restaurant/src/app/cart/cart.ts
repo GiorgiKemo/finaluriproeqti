73fd856b94c87b95f13df1423cb433995cf3cb6a
GiorgiKemo <giorgikemoklidze1998@gmail.com>
2025-09-08 01:44:50 +0400

    Initial commit

---
+++ online-restaurant/src/app/cart/cart.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { Api } from '../api';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { CartService } from './cart.service';
+
+@Component({
+  selector: 'app-cart',
+  imports: [CommonModule, FormsModule],
+  templateUrl: './cart.html',
+  styleUrl: './cart.css'
+})
+export class Cart implements OnInit {
+  cartItems: any[] = [];
+  totalPrice: number = 0;
+
+  constructor(private api: Api, private cartService: CartService) { }
+
+  ngOnInit(): void {
+    this.cartService.cartItems$.subscribe(items => {
+      this.cartItems = items;
+      this.calculateTotal();
+    });
+  }
+
+  updateQuantity(item: any): void {
+    this.api.updateBasket(item).subscribe(() => {
+      this.calculateTotal();
+    });
+  }
+
+  deleteItem(item: any): void {
+    const productId = item.productId || item.id;
+    if (productId) {
+      this.cartService.deleteItem(productId);
+      this.api.deleteProduct(productId).subscribe({
+        next: () => {
+          console.log('Item deleted successfully from backend.');
+        },
+        error: (err: any) => {
+          console.error('Error deleting item from backend:', err);
+        }
+      });
+    } else {
+      console.error('Could not find product id to delete.');
+    }
+  }
+
+  calculateTotal(): void {
+    this.totalPrice = this.cartItems.reduce((acc, item) => acc + (item.price * item.quantity), 0);
+  }
+}
